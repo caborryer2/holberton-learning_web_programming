82c7965e140d336b0f675516e93e272d62d4655a
caborryer2 <58482323+caborryer2@users.noreply.github.com>
2019-12-06 11:39:26 -0500

    Create 3-my_first_loop.js

---
+++ 5-javascript/3-my_first_loop.js
@@ -0,0 +1,10 @@
+function myFunction(){
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  var text = "";
+  var i = 0;
+  while (i< numbers.length) {
+    text += numbers[i] + "<br>";
+    i++;
+
+  }
+}
